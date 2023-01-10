3cdf742115a61babda413145add6d85b74dff2ca
blabaret <dennis.xiayue@kellogg.northwestern.edu>
2023-01-10 22:22:58 +0000

    Finished up to lab-4-2

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,15 @@
 -- How many lifetime hits does Barry Bonds have?
 
+SELECT * FROM players
+WHERE first_name = "Barry"
+AND last_name = "Bonds";
+
+SELECT first_name, last_name, SUM(hits)
+FROM stats
+INNER JOIN players ON players.id = stats.player_id
+WHERE first_name = "Barry" AND last_name = "Bonds"
+;
+
 -- Expected result:
 -- 2935
 
--- lab-4-2.sql
+++ lab-4-2.sql
@@ -1,6 +1,17 @@
 -- What are the first and last names of the players who 
 -- played for the 2020 Chicago Cubs?
 
+SELECT first_name, last_name
+FROM players
+INNER JOIN stats
+ON players.id = stats.player_id
+INNER JOIN teams
+ON teams.id = stats.team_id
+WHERE name = "Chicago Cubs"
+AND year = 2020
+;
+
+
 -- Hint: combine WHERE clauses using AND, e.g.
 -- WHERE column1 = expression1
 -- AND column2 = expression2
